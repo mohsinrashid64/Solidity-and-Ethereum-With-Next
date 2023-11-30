e2c6c2bfc451006ca2191c9a99ead7a1e047e093
Muhammad Mohsin Rashid <mohsinrashid64@gmail.com>
2023-11-30 08:10:41 +0500

    Lec-54-Withdraw-2nd-Commmit

--- faucet/contracts/Faucet.sol
+++ faucet/contracts/Faucet.sol
@@ -2,8 +2,62 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 
-contract Faucet{
-    uint public funds = 1000;
-    int public counter = -10;
-    uint32 public test = 4294967295;
+
+contract Faucet {
+
+  uint public numOfFunders;
+  mapping(address => bool) private funders;
+  mapping(uint => address) private lutFunders;
+
+  receive() external payable {}
+
+  function addFunds() external payable{
+    address funder = msg.sender;
+
+    if(!funders[funder]){
+      uint index = numOfFunders++;
+      funders[funder]=true;
+      lutFunders[index]=funder;
+    }
+  }
+
+  function withdraw(uint withdrawAmount) external {
+    if(withdrawAmount < 1000000000000000000){
+     payable(msg.sender).transfer(withdrawAmount); 
+    }
+
+  }
+
+  
+  function getAllFunders() external view returns (address[] memory) {
+    address[] memory _funders = new address[](numOfFunders);
+
+    for (uint i = 0; i < numOfFunders; i++) {
+      _funders[i] = lutFunders[i];
+    }
+
+    return _funders;
+  }
+
+  function getFunderAtIndex(uint8 index) external view returns(address) {
+    return lutFunders[index];
+  }
+
 }
+
+
+// COMMANDS TO RUN
+/*
+truffle migrate --reset
+truffle console
+
+const instance = await Faucet.deployed();
+
+instance.addFunds({from:accounts[0],value:"2000000000000000000"})
+instance.addFunds({from:accounts[1],value:"2000000000000000000"})
+
+instance.withdraw("500000000000000000",{from:accounts[1]})
+
+instance.getFunderAtIndex(0)
+instance.getAllFunders()
+*/
