ff9a6b45331494d6357905159d0bb23fa1dd42c3
Muhammad Mohsin Rashid <mohsinrashid64@gmail.com>
2023-12-02 11:02:48 +0500

    Update Faucet.sol

--- faucet/contracts/Faucet.sol
+++ faucet/contracts/Faucet.sol
@@ -8,13 +8,32 @@ contract Faucet {
   uint public numOfFunders;
   mapping(address => bool) private funders;
   mapping(uint => address) private lutFunders;
+  address public owner;
+
+  constructor(){
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner {
+    require(
+      msg.sender == owner,
+      "Only owner can call this function"
+    );
+    _;
+  }
 
   modifier limitWithdraw(uint withdrawAmount){
-    require(withdrawAmount < 100000000000000000, "Cannot withdraw more than 0.1 ether");
+    require(withdrawAmount < 100000000000000000, 
+    "Cannot withdraw more than 0.1 ether"
+    );
     _;
   }
 
   receive() external payable {}
+
+  function transferOwnership(address newOwner)external onlyOwner{
+    owner = newOwner;
+  }
 
   function addFunds() external payable{
     address funder = msg.sender;
@@ -24,6 +43,14 @@ contract Faucet {
       funders[funder]=true;
       lutFunders[index]=funder;
     }
+  }
+
+  function test1() external onlyOwner{
+    // some mangaing stuff that only admin should have access to
+  }
+
+  function test2() external onlyOwner{
+    // some mangaing stuff that only admin should have access to
   }
 
   function withdraw(uint withdrawAmount) external limitWithdraw(withdrawAmount){
@@ -58,6 +85,8 @@ const instance = await Faucet.deployed();
 
 instance.addFunds({from:accounts[0],value:"2000000000000000000"})
 instance.addFunds({from:accounts[1],value:"2000000000000000000"})
+
+instance.test1({from:accounts[0]})
 
 instance.withdraw("500000000000000000",{from:accounts[1]})
 
