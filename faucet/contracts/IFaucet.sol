8e7a96388e29c819e4c44151001ffd17170de6df
Muhammad Mohsin Rashid <mohsinrashid64@gmail.com>
2023-12-02 12:52:43 +0500

    Lec-60-Interface-1st-commit

---
+++ faucet/contracts/IFaucet.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+// they cannot inherit from other smart contracts
+// they can only inherit from other interfaces
+// they cannot delcare a constructor
+// they cannot declare state variables
+// all declared functions have to be external
+
+
+
+interface IFaucet{
+
+
+    function addFunds() external payable;
+    function withDraw(uint withdrawAmount) external;
+}
---
+++ faucet/contracts/Logger.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+// It's a way for desiner to say that
+// "any child of the abstract contract has to implement the specified methods
+
+abstract contract Logger{
+
+    uint public testNum;
+
+    constructor(){
+        testNum = 1000;
+    }
+
+    function emitLog() public pure virtual returns(bytes32);
+
+    function test3() external pure returns(uint){
+        return 100;
+    }
+
+}
