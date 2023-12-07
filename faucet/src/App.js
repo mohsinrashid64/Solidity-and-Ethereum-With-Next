36b48ca84f68df51c0633ed316b9d1269c8a5c3f
Muhammad Mohsin Rashid <mohsinrashid64@gmail.com>
2023-12-07 08:28:32 +0500

    MetaMask-Web3-Integration-App-1st-commmit

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -1,75 +1,78 @@
-import { useEffect } from "react";
-import "./App.css"
+import React, { useEffect, useState } from "react";
 import Web3 from "web3";
-
-import { useState } from "react";
-import detectEthereumProvider from '@metamask/detect-provider'
+import detectEthereumProvider from "@metamask/detect-provider";
+import "./App.css";
 
 function App() {
-
-
-
   const [web3Api, setWeb3Api] = useState({
     provider: null,
-    web3: null
-  })
+    web3: null,
+  });
 
-  const[accounts,setAccounts] = useState(null)
+  const [accounts, setAccounts] = useState(null);
+  const [balance, setBalance] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     const loadProvider = async () => {
-      const provider = await detectEthereumProvider()
+      const provider = await detectEthereumProvider();
 
-      if(provider){
+      if (provider) {
         setWeb3Api({
           web3: new Web3(provider),
-          provider
-        })
+          provider,
+        });
       } else {
-        console.error("Please Install MetaMask")
+        console.error("Please Install MetaMask");
       }
-    }
-    loadProvider()
-  },[])
+    };
+    loadProvider();
+  }, []);
 
-  useEffect(()=>{
-    const getAccount = async () =>{
-        const accounts = await web3Api.web3.eth.getAccounts();
-        setAccounts(accounts[0])
-    }
-    web3Api.web3 && getAccount()
-  },[web3Api.web3])
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccounts(accounts[0]);
+    };
+
+    const getBalance = async () => {
+      if (accounts) {
+        const balance = await web3Api.web3.eth.getBalance(accounts);
+        // Convert balance from wei to ether
+        const etherBalance = web3Api.web3.utils.fromWei(balance, "ether");
+        setBalance(etherBalance);
+      }
+    };
+
+    web3Api.provider && getAccount();
+    accounts && getBalance();
+  }, [web3Api.web3, web3Api.provider, accounts]);
 
   return (
     <>
-      <div className = "faucet-wrapper">
-        <div className = "faucet">
-        <div className="is-flex align-items">            
-
-          <span>
-            <strong className="mr-2">Account:</strong>
-          </span>
-          {accounts ?
-            <span>{accounts}</span>  :
-             <button 
-              className="button is-small mr-2"
-              onClick={() => web3Api.provider.request({method: "eth_requestAccounts"})}
-             >
-              Connect Wallet
-             </button>
-            }
-          </div>
-          {accounts}
-          <div className = "balance-view is-size-2 mb-8 mt-8">
-            Current Balance: <strong>10</strong> ETH
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="is-flex align-items">
+            <span>
+              <strong className="mr-2">Account:</strong>
+            </span>
+            {accounts ? (
+              <span>{accounts}</span>
+            ) : (
+              <button
+                className="button is-small mr-2"
+                onClick={() => web3Api.provider.request({ method: "eth_requestAccounts" })}
+              >
+                Connect Wallet
+              </button>
+            )}
           </div>
 
-          <button className="button is-link mr-2">Donate</button>
-          <button className="button is-primary ">Withdraw</button>
-        </div> 
-      </div> 
+          <div className="balance-view is-size-2 mb-8 mt-8">
+            Current Balance: <strong>{balance !== null ? `${balance} ETH` : "Loading..."}</strong>
+          </div>
+        </div>
+      </div>
     </>
-    
   );
 }
 
